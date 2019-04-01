efb69b111825d8f34b0a99979c3da7317271a09a
Thibault <ThibaultFaouz@gmail.com>
2019-04-01 18:28:39 -0400

    new

---
+++ liri.js
@@ -0,0 +1,39 @@
+// error won't allow npm of axios, dotenv, moment, keys
+
+var keys = require("./keys.js");
+
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+
+var name = process.argv.slice(3).join(" ");
+console.log("working");
+
+
+if ((command === "movie-this") && (!name) {
+    movieThingdefault = "Mr.Nobody";
+    console.log("It's on Netflix!");
+} else if movieCommand((command === "movie-this")) {
+    //omdb*()
+} else if (command === "concert-this") {
+    //bandthing
+    console.log("stuff");
+} else if (commamd === "spotify-this-song") {
+    // spotifyCommand();
+    console.log("spotify test");
+
+} else {
+    console.log("catch all ");
+}
+
+//function spotify
+function spotify(command, name)
+
+
+//function omdb
+
+//function band
